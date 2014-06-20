ee0bfc735940616e405e5ad5574e4aec908a47e2
Steve Yadlowsky <grizlo42@gmail.com>
2014-06-20 16:54:22 -0700

    This change adds a call model to count vector

--- matrix_generators/call_vector.py
+++ matrix_generators/call_vector.py
@@ -6,6 +6,14 @@ def get_counts_in_tower(start_time, interval):
     c = connection.cursor()
 
     query = """
+    SELECT COUNT(DISTINCT traj.timesta)
+    FROM mivehdetailedtrajectory traj
+    WHERE traj.timesta >= %s AND traj.timesta <= %s
+    """
+    c.execute(query, (start_time, start_time+interval))
+    intervals = float(c.fetchone()[0])
+
+    query = """
     SELECT COUNT(traj.oid)
     FROM cell_data_tower tower, mivehdetailedtrajectory traj
     WHERE traj.timesta >= %s AND traj.timesta <= %s
@@ -13,8 +21,8 @@ def get_counts_in_tower(start_time, interval):
     GROUP BY tower.id
     ORDER BY tower.id
     """
-
     c.execute(query, (start_time, start_time+interval))
-    tower_counts = [r[0] for r in c]
+
+    tower_counts = [r[0]/intervals for r in c]
     
-    print tower_counts
+    return np.array([np.random.poisson(interval*count/3960.0) for count in tower_counts])

65b25e59e843f02568de94b0f2fe43cdc8c099fd
Steve Yadlowsky <grizlo42@gmail.com>
2014-06-21 02:28:07 -0700

    Try lighter weight query

--- matrix_generators/density_vector.py
+++ matrix_generators/density_vector.py
@@ -16,14 +16,22 @@ def density_vector(start_time, interval):
     intervals = float(c.fetchone()[0])
 
     query = """
-    SELECT 
-      (SELECT COUNT(traj.oid)
-       FROM mivehdetailedtrajectory traj
-       WHERE traj.sectionId = l.id
-       AND traj.timesta >= %s AND traj.timesta <= %s)
-    FROM microsim_link l
-    ORDER BY l.id
+    SELECT id, ROW_NUMBER() OVER (ORDER BY id)
+    FROM microsim_link
+    """
+    c.execute(query)
+    row_numbers = {x[0]: x[1] for x in c}
+
+    query = """
+    SELECT traj.sectionId, COUNT(traj.oid)
+    FROM mivehdetailedtrajectory traj
+    WHERE traj.timesta >= 26589.2 AND traj.timesta <= 26589.2+300
+    GROUP BY traj.sectionId                                           
+    ORDER BY traj.sectionid;
     """
     c.execute(query, (start_time, start_time+interval))
 
-    link_counts = np.array([r[0]/intervals for r in c])
+    link_counts = np.zeros(len(row_numbers.keys()))
+    for r in c:
+        link_counts[row_numbers[r[0]]] = r[1]
+    return link_counts
